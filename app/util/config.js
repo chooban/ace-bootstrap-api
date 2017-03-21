0344c412741ca4573805d63393aad7883e024cc6
Ross Hendry <rhendry@googlemail.com>
2017-03-21 16:58:01 +0000

    Make the domain into a secret

--- app/util/config.js
+++ app/util/config.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 
 const config = {
-  domain: 'acemyorder.eu.auth0.com',
+  domain: '',
   clientId: ''
 };
 
 if (process.env.SECRET) {
   config.clientId = process.env.CLIENT_ID;
+  config.domain = process.env.DOMAIN;
 } else {
   config.clientId = fs.readFileSync('/run/secrets/auth0_client_id', 'utf8').trim();
+  config.domain = fs.readFileSync('/run/secrets/auth0_domain', 'utf8').trim();
 }
 
 module.exports = config;
